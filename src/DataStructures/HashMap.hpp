0deab180c942c4e31a5710a1fe14fadb75be0ef9
pbudzyns <pawel.budzynski19+git@gmail.com>
2023-08-28 00:20:39 +0200

    add hashtable resize

--- src/DataStructures/HashMap.hpp
+++ src/DataStructures/HashMap.hpp
@@ -36,6 +36,7 @@ class LinkedList {
   void insertKeyValue(const K& key, const V& value);
   bool removeKey(const K& key);
   ListNode<K, V>* find(const K& key);
+  ListNode<K, V>* getRoot() { return m_Root; }
 
  private:
   ListNode<K, V>* m_Root;
@@ -60,7 +61,9 @@ template <typename K, typename V>
 class HashMap {
  public:
   HashMap()
-      : m_Size{0}, m_Capacity{2}, m_Table{new LinkedList<K, V>*[m_Capacity]} {
+      : m_Size{0},
+        m_TableCapacity{2},
+        m_Table{new LinkedList<K, V>*[m_TableCapacity]} {
     initTable();
   }
   ~HashMap() { delete[] m_Table; }
@@ -72,16 +75,23 @@ class HashMap {
  private:
   LinkedList<K, V>** m_Table;
   HashFunction<K> m_HashFn;
-  size_type m_Capacity;
+  size_type m_TableCapacity;
   size_type m_Size;
   void initTable();
+  void resizeTable(size_type newTableCapacity);
   inline size_type getKeyIndex(const K& key);
 };
 
+// ------ HashMap Implementation ----------------------
+
 template <typename K, typename V>
 void HashMap<K, V>::insert(const K& key, const V& value) {
+  if (m_Size + 1 >= m_TableCapacity) {
+    resizeTable(m_TableCapacity * 2);
+  }
   size_type index{getKeyIndex(key)};
   m_Table[index]->insertKeyValue(key, value);
+  ++m_Size;
 }
 
 template <typename K, typename V>
@@ -91,6 +101,7 @@ void HashMap<K, V>::remove(const K& key) {
   if (!list->removeKey(key)) {
     throw std::out_of_range("Key not found!");
   }
+  --m_Size;
 }
 
 template <typename K, typename V>
@@ -114,15 +125,37 @@ bool HashMap<K, V>::includes(const K& key) {
 
 template <typename K, typename V>
 void HashMap<K, V>::initTable() {
-  for (size_type i{0}; i < m_Capacity; ++i) {
+  for (size_type i{0}; i < m_TableCapacity; ++i) {
     m_Table[i] = new LinkedList<K, V>;
   }
 }
 
 template <typename K, typename V>
-inline size_type HashMap<K, V>::getKeyIndex(const K& key) {
-  return m_HashFn(key, m_Capacity);
+void HashMap<K, V>::resizeTable(size_type newTableCapacity) {
+  LinkedList<K, V>** oldTable = m_Table;
+  size_type oldTableCapacity = m_TableCapacity;
+
+  m_Table = new LinkedList<K, V>*[newTableCapacity];
+  m_TableCapacity = newTableCapacity;
+  m_Size = 0;
+  initTable();
+
+  for (size_type i{0}; i < oldTableCapacity; ++i) {
+    ListNode<K, V>* node = oldTable[i]->getRoot();
+    while (node) {
+      insert(node->getKey(), node->getValue());
+      node = node->getNext();
+    }
+  }
+  delete[] oldTable;
 }
+
+template <typename K, typename V>
+inline size_type HashMap<K, V>::getKeyIndex(const K& key) {
+  return m_HashFn(key, m_TableCapacity);
+}
+
+// ------ LinkedList Implementation ----------------------
 
 template <typename K, typename V>
 LinkedList<K, V>::~LinkedList() {
