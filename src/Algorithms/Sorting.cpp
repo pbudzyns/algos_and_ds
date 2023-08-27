f4234b2da3cff3977dbaa21c173d5d33f01f5237
pbudzyns <pawel.budzynski19+git@gmail.com>
2023-08-27 14:24:53 +0200

    commit ugly

--- src/Algorithms/Sorting.cpp
+++ src/Algorithms/Sorting.cpp
@@ -1,5 +1,8 @@
 #include "Sorting.h"
 
-int Sort::Min(int v1,
-int v2) { return v1 < v2 ? 
-v1 : v2; }
+int Sort::Min(int
+
+                  v1,
+              int v2) {
+  return v1 < v2 ? v1 : v2;
+}
