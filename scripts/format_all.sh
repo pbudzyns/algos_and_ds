6513887ef790d6c657f3878d058b4717f1333a31
pbudzyns <pawel.budzynski19+git@gmail.com>
2023-08-27 14:47:08 +0200

    update pre-commit

---
+++ scripts/format_all.sh
@@ -0,0 +1,10 @@
+source_files=$(find src -type f -regex ".*\.\(cpp\|cxx\|h\|hpp\)")
+test_files=$(find tests -type f -regex ".*\.\(cpp\|cxx\|h\|hpp\)")
+
+if [ ! -z "$source_files" ]; then
+    clang-format -i --style=Google $source_files
+fi
+
+if [ ! -z "$test_files" ]; then
+    clang-format -i --style=Google $test_files
+fi
